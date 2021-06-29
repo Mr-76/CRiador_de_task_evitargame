45b93bde780c41e21bf2b0f32481ea73590593d3
holand76 <67565234+holand76@users.noreply.github.com>
2021-06-29 13:03:23 -0400

    Add files via upload

---
+++ Part1.py
@@ -0,0 +1,48 @@
+import subprocess
+import os
+import sys
+import time
+
+MODULOS = ("pip install datetime psutil")   
+
+comandLineCode =  subprocess.Popen(MODULOS)
+
+cwd = os.getcwd()  #script path
+
+
+print(sys.executable.replace('python.exe', 'pythonw.exe'))
+
+exePath = sys.executable.replace('python.exe', 'pythonw.exe')
+
+
+LINHA_SCRIPT_USAR = ( (exePath) + " " + r'"{}\STUP.py"'.format(cwd) )
+print(LINHA_SCRIPT_USAR)
+
+
+nome = "PARTT2.bat"
+f = open(nome, "w")
+f.write(LINHA_SCRIPT_USAR)
+f.close()
+
+time.sleep(5)
+
+#####
+cwd = os.getcwd()  #script path
+print(cwd)
+
+
+#C:/Users/vluca/AppData/Local/Microsoft/WindowsApps/PythonSoftwareFoundation.Python.3.8_qbz5n2kfra8p0/python.exe "Z:\python\Untitled-1.py"
+print(sys.executable.replace('python.exe', 'pythonw.exe'))
+
+exePath = sys.executable.replace('python.exe', 'pythonw.exe')
+
+
+LINHA_SCRIPT_USAR = ( (exePath) + " " + r'"{}\teste_task_grande.pyw"'.format(cwd) )
+
+print(LINHA_SCRIPT_USAR)#inside bat file
+
+nome = "Executavel_game.bat"
+f = open(nome, "w")
+f.write(LINHA_SCRIPT_USAR)
+f.close()
+
---
+++ STUP.py
@@ -0,0 +1,37 @@
+#Adiciona a task ao windows
+
+import subprocess #usar o terminal 
+import os
+import psutil
+import sys
+import time
+from datetime import datetime
+
+path = os.path.realpath(__file__)
+
+
+#need to read a file withj the path
+
+nome = "Executavel_game.bat"
+
+path = path.replace("STUP.py","")
+
+
+scriptPath = r'"{}{}"'.format(path,nome) #colocar isso no task
+
+print(scriptPath,"path")
+
+#rodar 1 vez 
+
+
+#e tb comando de criaçao da task
+
+PRIMEIRO_COD_TASK = (r'SCHTASKS /CREATE /SC ONLOGON /TN "GAME_NEVER" /TR ')
+SEGUNDO_COD_TASK = scriptPath
+print(SEGUNDO_COD_TASK)
+
+FINAL_COD_TASK = PRIMEIRO_COD_TASK + SEGUNDO_COD_TASK
+
+print(FINAL_COD_TASK)
+
+subprocess.Popen(FINAL_COD_TASK,shell = True)
